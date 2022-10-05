561d8f1ad649d35b186e993decf4d6d8e37b2f39
Clay89tms <bboyClay89@gmail.com>
2022-10-05 19:16:27 +0300

    ДЗ 5 2 часть, 1 часть (Полный)

--- src/Main.java
+++ src/Main.java
@@ -2,22 +2,36 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
         Scanner sc = new Scanner(System.in);
-        Integer DZ = sc.nextInt();
+        Integer dz5 = null;
+        do {
+            dz5 = 0;
+            System.out.print("Введите номер ДЗ 05 (№1-4) : ");
+            if (sc.hasNextInt()) {
+                dz5 = sc.nextInt();
+                switch (dz5) {
+                    case (1):
+                        dz5_01();
+                        break;
+                    case (2):
+                        dz5_02();
+                        break;
+                    case (3):
+                        dz05_03();
+                        break;
+                    case (4):
+                        dz05_04();
+                        break;
+                    default:
+                        System.out.println("нет такого ДЗ, попробуйте снова.");
+                }
 
-        switch (DZ) {
-            case (1):
-            case (2):
-            case (3):
-                dz05_03();
+            } else {
+                System.out.println("Ввели не число...");
                 break;
-            case (4):
-                dz05_04();
-                break;
-            default:
-                System.out.println();
-        }
+            }
+        } while (dz5 < 5);
+        sc.close();
     }
 
     private static void dz05_04() {
@@ -78,5 +92,80 @@ public class Main {
         }
         System.out.println("Сумма всех элементов = " + sum);
 
+    }
+
+    private static void dz5_02() {
+        System.out.println("Шахматная доска\n" +
+                "Создать программу для раскраски шахматной доски с помощью цикла.\n" +
+                "Создать двумерный массив String'ов 8х8. С помощью циклов задать\n" +
+                "элементам циклам значения B(Black) или W(White). Результат работы\n" +
+                "программы:\n" +
+                "W B W B W B W B\n" +
+                "B W B W B W B W\n" +
+                "W B W B W B W B\n" +
+                "B W B W B W B W\n" +
+                "W B W B W B W B\n" +
+                "B W B W B W B W\n" +
+                "W B W B W B W B\n" +
+                "B W B W B W B W");
+        String chess[][] = new String[8][8];
+        for (int i = 0; i < chess.length; i++) {
+            for (int j = 0; j < chess[i].length; j += 2) {
+                if ((i == 0) || (i % 2 == 0)) {
+                    chess[i][j] = "W";
+                    chess[i][j + 1] = "B";
+                } else {
+                    chess[i][j + 1] = "W";
+                    chess[i][j] = "B";
+                }
+            }
+        }
+        System.out.println("Выводим таблицу : ");
+        for (int i = 0; i < chess.length; i++) {
+            for (int j = 0; j < chess[i].length; j++) {
+                System.out.print(chess[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+
+    private static void dz5_01() {
+        System.out.println("(01): \nСоздать трехмерный массив из целых чисел.\n" +
+                "С помощью циклов \"пройти\" по всему массиву и увеличить каждый\n" +
+                "элемент на заданное число. Пусть число, на которое будет\n" +
+                "увеличиваться каждый элемент, задается из консоли.");
+
+        int mas3[][][] = new int[5][5][5];
+        System.out.println("Изначальный рандомный массив : ");
+        for (int a = 0; a < mas3.length; a++) {
+            for (int b = 0; b < mas3[a].length; b++) {
+                System.out.print("{");
+                for (int c = 0; c < mas3[a][b].length; c++) {
+                    mas3[a][b][c] = (int) (Math.random() * 9);
+                    System.out.print(mas3[a][b][c] + " ");
+                }
+                System.out.print("}\t");
+            }
+            System.out.println();
+        }
+        Scanner sc = new Scanner(System.in);
+        Integer chis = null;
+        System.out.print("Введите число на которое увеличить все эллементы массива : ");
+        if (sc.hasNextInt()) {
+            chis = sc.nextInt();
+            System.out.println("Измененный массив (" + chis + ") : ");
+            for (int a = 0; a < mas3.length; a++) {
+                for (int b = 0; b < mas3[a].length; b++) {
+                    System.out.print("{");
+                    for (int c = 0; c < mas3[a][b].length; c++) {
+                        mas3[a][b][c] += chis;
+                        System.out.print(mas3[a][b][c] + " ");
+                    }
+                    System.out.print("}\t");
+                }
+                System.out.println();
+            }
+        }
     }
 }
